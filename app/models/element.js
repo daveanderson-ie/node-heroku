843a937a56aec1eb9b3eb8314f491a166b801837
Dave Anderson <github@gingertechie.com>
2019-01-19 15:16:56 +0000

    Update element.js

--- app/models/element.js
+++ app/models/element.js
@@ -1,44 +1,30 @@
-const { Pool, Client } = require('pg');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 async function getElements(){
-    const pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'node-heroku-test',
-        password: '',
-        port: 5432,
-    });
-
-    let p = pool.query('SELECT id, name FROM elements').then((res) => {
+    client.connect();
+    let p = client.query('SELECT id, name FROM elements').then((res) => {
         return res.rows;
     }).catch((err)=>{
         console.log('Error', err);
         return err;
     });
     let result = await p;
-
-    pool.end();
     return result;
 }
 
 async function addElement(name){
-    const pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'node-heroku-test',
-        password: '',
-        port: 5432,
-    });
-
-    let p = pool.query("INSERT into ELEMENTS (name) values ('"+name+"')").then((res) => {
+    let p = client.query("INSERT into ELEMENTS (name) values ('"+name+"')").then((res) => {
         return name;
     }).catch((err)=>{
         console.log('Error', err);
         return err;
     });
     let result = await p;
-
-    pool.end();
     return result;
 };
 
